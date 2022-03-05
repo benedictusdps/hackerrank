6eeae742d42a7fe63c01613fda477d730a8bdcc3
benedictusdps <benedictusdps@gmail.com>
2022-03-05 18:39:13 +0700

    Add files from upload

---
+++ Tutorial-30-Days-of-Code/Day-1/data-types.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+# Sample input:
+# 12
+# 4.0
+# "is the best place to learn and practice coding!"
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i + i2)
+
+# Print the sum of the double variables on a new line.
+print(d + d2)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + s2)
