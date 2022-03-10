7566f9650ed361d0376eb107307bd9955b8fe80a
benedictusdps <benedictusdps@gmail.com>
2022-03-10 09:30:33 +0700

    Add files from upload

---
+++ Tutorial-30-Days-of-Code/Day-8/dictionaries-and-maps.py
@@ -0,0 +1,31 @@
+# Given n names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. 
+# You will then be given an unknown number of names to query your phone book for. 
+# For each  queried, print the associated entry from your phone book on a new line in the form name=phoneNumber; 
+# if an entry for  is not found, print Not found instead.
+
+# Number of key-value pairs
+n = int(input())
+
+# Declare both dictionary and list
+phone_book = dict()
+names = []
+
+# Loop through the number of pairs, appending the input into the names list
+for i in range(n):
+    names.append(input().split())
+
+# Loop through the list, inserting the key and val into the phone book dictionary
+for key, val in names:
+    phone_book[key] = val
+
+# While there is still name input, check in the phonebook if the name existed
+# If it exist, print out the name and number
+while True:
+    try:
+        name = input()
+        if name in phone_book:
+            print("%s=%s" % (name, phone_book[name]))
+        else:
+            print("Not found")
+    except:
+        break
