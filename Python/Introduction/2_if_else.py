c9a57b4a82f6f4815ce709e78f8203235f959d45
benedictusdps <benedictusdps@gmail.com>
2022-03-13 23:23:16 +0700

    Add files from upload

--- Python/Introduction/2_if_else.py
+++ Python/Introduction/2_if_else.py
@@ -1,4 +1,5 @@
 # Given an integer, n, perform the following conditional actions:
+
 # If n is odd, print Weird
 # If n is even and in the inclusive range of 2 to 5, print Not Weird
 # If n is even and in the inclusive range of 6 to 20, print Weird
@@ -22,7 +23,8 @@ if __name__ == '__main__':
             print("Not Weird")
         elif n >= 6 and n <= 20:
             print("Weird")
-        elif n > 20:
-            print("Not Weird")          
+        else:
+            print("Not Weird")
     else:
         print("Weird")
+
